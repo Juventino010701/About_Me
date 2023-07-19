7e9f979331611ad15e6480cadfbcccf9bd202fb8
Juventino010701 <sindartojuventino@gmail.com>
2023-07-19 20:17:13 +0700

    update profession

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Copywriter", "Web Developer", "UI/UX Designer", "Investor"],
+        strings: ["Copywriter", "Web Developer", "UI Designer", "Investor"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Copywriter", "Web Developer", "UI/UX Designer", "Investor"],
+        strings: ["Copywriter", "Web Developer", "UI Designer", "Investor"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
